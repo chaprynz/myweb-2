e0736ca5a5349b2d4164eb169d9e7573c687703c
chaprynz <henrysai89@gmail.com>
2016-03-15 12:08:39 +0000

    signin signout

--- Gemfile
+++ Gemfile
@@ -3,12 +3,14 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.2.5'
 gem 'sass-rails', '~> 5.0'
+gem "font-awesome-rails"
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'devise', '~> 3.5', '>= 3.5.6'
 group :development, :test do
   gem 'byebug'
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,34 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+// Add following lines.
+//= require jquery 
+//= require jquery_ujs
+
+function w3_open() {
+    document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
+    document.getElementsByClassName("w3-overlay")[0].style.display = "block";
+}
+function w3_close() {
+    document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
+    document.getElementsByClassName("w3-overlay")[0].style.display = "none";
+}
+
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("myTop").classList.add("w3-card-4");
+        document.getElementById("myIntro").classList.add("w3-show-inline-block");
+    } else {
+        document.getElementById("myIntro").classList.remove("w3-show-inline-block");
+        document.getElementById("myTop").classList.remove("w3-card-4");
+    }
+}
+
+function myFunc(id) {
+    document.getElementById(id).classList.toggle("w3-show");
+    document.getElementById(id).previousElementSibling.classList.toggle("w3-theme");
+}
+
+
